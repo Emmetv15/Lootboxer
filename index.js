2d82b6683fed8e23f8dce881b15a0532e4fcf1c5
Emmet_v15 <emmettrey726@gmail.com>
2020-10-23 20:51:02 +0100

    using discord collections for commands

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 const fs = require('fs');
-var store = JSON.parse(fs.readFileSync('./store.json'));
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+    client.user.setActivity(`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} Lootboxers`, { type: "WATCHING" })
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+    
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Error!');
+    }
 });
 
-client.login(store.token);
+client.login(token);
