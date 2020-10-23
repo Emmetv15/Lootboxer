edd7fc8994c9f77a7f271ea9c10ec268a67571c4
Emmet_v15 <emmettrey726@gmail.com>
2020-10-23 21:52:58 +0100

    takeoff #2

--- commands/role.js
+++ commands/role.js
@@ -1,7 +1,8 @@
 module.exports = {
-	name: 'ping',
-    description: 'Ping!',
+	name: 'role',
+    description: 'Used to give yourself a role',  
     args: false,
+    usage: '<user> <role>',
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},
