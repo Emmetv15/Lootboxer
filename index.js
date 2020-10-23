6f7b4d3c7be16aaf1aba0d12ee2799be8beeefbd
Emmet_v15 <emmettrey726@gmail.com>
2020-10-23 20:28:21 +0100

    takeoff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const fs = require('fs');
 var store = JSON.parse(fs.readFileSync('./store.json'));
 
 client.on('ready', () => {
